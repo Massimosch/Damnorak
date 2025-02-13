cfde255968d5573ad5dd5afe337cb808c70a4203
Mico <120001621+Massimosch@users.noreply.github.com>
2025-02-13 16:57:03 +0200

    Pathfinding

--- Assets/Scripts/Pathfinding/Pathfinding.cs
+++ Assets/Scripts/Pathfinding/Pathfinding.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Pathfinding : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    GridA grid;
 
-    // Update is called once per frame
-    void Update()
+    void Awake()
     {
-        
+        grid = GetComponent<GridA>();
     }
+    void FindPath(Vector3 startPos, Vector3 targetPos)
+    {
+        Node startNode = grid.NodeFromWorldPoint(startPos);
+        Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+        List<Node> openSet = new();
+        HashSet<Node> closedSet = new();
+        openSet.Add(startNode);
+
+        while (openSet.Count > 0)
+        {
+            Node currentNode = openSet[0];
+            for (int i = 1; i < openSet.Count; i++)
+            {
+                if (openSet[i].fCost < currentNode.fCost || openSet[i].fCost == currentNode.fCost && openSet[i].hCost < currentNode.hCost)
+                {
+                    currentNode = openSet[i];
+                }
+            }
+
+            openSet.Remove(currentNode);
+            closedSet.Add(currentNode);
+
+            if (currentNode == targetNode)
+            {
+                return;
+            }
+            
+        }
+    }
+        
+    
 }
